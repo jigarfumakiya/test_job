55aeab82d375dd662c932f69d23a22d3c1250638
Jigar Fumakiya <jigarfumakiya@users.noreply.github.com>
2022-07-24 19:22:05 +0530

    Added Button.

--- lib/features/practitioner/presentation/pages/create_appointment.dart
+++ lib/features/practitioner/presentation/pages/create_appointment.dart
@@ -10,64 +10,81 @@ class CreateAppointment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return MediaQuery(
       data: const MediaQueryData(padding: EdgeInsets.zero),
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                appLocaleInstance().createAppointment,
-                style: AppTextStyles.blackBoldTextStyle24(),
+        controller: ScrollController(),
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  appLocaleInstance().createAppointment,
+                  style: AppTextStyles.blackBoldTextStyle24(),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Wrap(
-                spacing: 8,
-                runSpacing: 8,
-                children: [
-                  AppChoiceChip(
-                    selected: true,
-                    text: appLocaleInstance().general,
-                  ),
-                  AppChoiceChip(
-                    selected: false,
-                    text: appLocaleInstance().group,
-                  ),
-                  AppChoiceChip(
-                    selected: false,
-                    text: appLocaleInstance().blockSlot,
-                  ),
-                  AppChoiceChip(
-                    selected: false,
-                    text: appLocaleInstance().advanced,
-                  ),
-                  AppChoiceChip(
-                    selected: false,
-                    text: appLocaleInstance().walkIn,
-                  ),
-                ],
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: [
+                    AppChoiceChip(
+                      selected: true,
+                      text: appLocaleInstance().general,
+                    ),
+                    AppChoiceChip(
+                      selected: false,
+                      text: appLocaleInstance().group,
+                    ),
+                    AppChoiceChip(
+                      selected: false,
+                      text: appLocaleInstance().blockSlot,
+                    ),
+                    AppChoiceChip(
+                      selected: false,
+                      text: appLocaleInstance().advanced,
+                    ),
+                    AppChoiceChip(
+                      selected: false,
+                      text: appLocaleInstance().walkIn,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            _buildTitle(
-                selected: true, title: appLocaleInstance().practitioners),
-            Divider(color: AppColors.geryColor, height: 2),
-            _buildTitle(
-                selected: false, title: appLocaleInstance().profileDateTime),
-            Divider(color: AppColors.geryColor, height: 2),
-            _buildTitle(
-                selected: false, title: appLocaleInstance().servicesPayment),
-            Divider(color: AppColors.geryColor, height: 2),
-            _buildTitle(selected: false, title: appLocaleInstance().patient),
-            Divider(color: AppColors.geryColor, height: 2),
-            _buildTitle(selected: false, title: appLocaleInstance().notes),
-          ],
+              const SizedBox(height: 20),
+              _buildTitle(
+                  selected: true, title: appLocaleInstance().practitioners),
+              Divider(color: AppColors.geryColor, height: 2),
+              _buildTitle(
+                  selected: false, title: appLocaleInstance().profileDateTime),
+              Divider(color: AppColors.geryColor, height: 2),
+              _buildTitle(
+                  selected: false, title: appLocaleInstance().servicesPayment),
+              Divider(color: AppColors.geryColor, height: 2),
+              _buildTitle(selected: false, title: appLocaleInstance().patient),
+              Divider(color: AppColors.geryColor, height: 2),
+              _buildTitle(selected: false, title: appLocaleInstance().notes),
+              Divider(color: AppColors.geryColor, height: 2),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5),
+                child: ElevatedButton(
+                  onPressed: null,
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 55)),
+                  child: Text(appLocaleInstance().create),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
